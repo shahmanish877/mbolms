46230e012247dee9c88ffe01b24f44879a86c233
shahmanish877 <shahmanish877@gmail.com>
2022-03-30 14:58:14 +0545

    Loan CRUD added

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -10,6 +10,10 @@ use Illuminate\Support\Facades\Gate;
 
 class LoanController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
     /**
      * Display a listing of the resource.
      *
